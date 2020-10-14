115ade4aa82fdbe8a69581a748f4a531f2ab1d89
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-10-14 10:17:08 +0200

    basic 1

---
+++ basic1.js
@@ -0,0 +1,3 @@
+var obj = { key: "value", a: 1 };
+console.log(obj.a); // 1
+console.log(obj['key']); // "value"
---
+++ basic1.ts
@@ -0,0 +1,14 @@
+class Person{
+	private firstName = "John";
+	private lastName = "Doe";
+	
+	constructor(firstName, lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		console.log(person);
+		// code...
+	}
+	name (){
+		return "Hi my name is "
+	}
+}
