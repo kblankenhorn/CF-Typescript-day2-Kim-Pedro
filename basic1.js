8fadc8e75bd7822a9952e82797f890a16591fda5
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-10-14 12:04:09 +0200

    basic 1

--- basic1.js
+++ basic1.js
@@ -1,3 +1,22 @@
-var obj = { key: "value", a: 1 };
-console.log(obj.a); // 1
-console.log(obj['key']); // "value"
+"use strict";
+var Person = /** @class */ (function () {
+    function Person(firstName, lastName, job, age) {
+        this.firstName = "";
+        this.lastName = "";
+        this.job = "";
+        this.age = "";
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.job = job;
+        this.age = age;
+    }
+    Person.prototype.name = function () {
+        return "Hi my name is " + this.firstName + " " + this.lastName + " " + this.job + " " + this.age;
+    };
+    Person.prototype.whoAreYou = function () {
+        return "Hi, " + this.name();
+    };
+    return Person;
+}());
+var fullName = new Person("Mario", "Nelson", "IT", "30");
+console.log(fullName.whoAreYou());
--- basic1.ts
+++ basic1.ts
@@ -1,14 +1,27 @@
+"use strict"
 class Person{
-	private firstName = "John";
-	private lastName = "Doe";
+	firstName = "";
+	lastName = "";
+	job = "";
+	age = ""
+
 	
-	constructor(firstName, lastName) {
+	constructor(firstName, lastName, job, age) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		console.log(person);
-		// code...
+		this.job = job;
+		this.age = age;
 	}
+
 	name (){
-		return "Hi my name is "
+		return `Hi my name is ${this.firstName} ${this.lastName} ${this.job} ${this.age}`;
+	}
+	whoAreYou() {
+		return `Hi, ${this.name()}`;
 	}
 }
+	
+	 let fullName = new Person("Mario", "Nelson", "IT", "30");
+     console.log(fullName.whoAreYou());
+
+
